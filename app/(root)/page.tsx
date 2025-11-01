e3fb207c17fb523c6b155d53300904f73338a6e2
Greg <47268138+gregsypek@users.noreply.github.com>
2025-11-01 16:32:05 +0100

    Create Next App with initial setup and assets

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+// export const metadata = {
+//   title: "Home ",
+// };
+
+const HomePage = () => {
+  return ( <>
+  <h1 className="my-9">hello</h1>
+  <Button className="destructive">buttodn</Button>
+  </> );
+}
+ 
+export default HomePage;
---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Bakery Store";
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESC ||  "A modern ecomerce platform for bakery products.";
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL ||"http://localhost:3000"
