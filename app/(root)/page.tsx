04d17871b91763c32e98e6268da8181d8b74738d
Greg <47268138+gregsypek@users.noreply.github.com>
2025-11-03 16:55:15 +0100

    Add Sample Products and Product List

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,21 @@
 import { Button } from "@/components/ui/button";
 
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+// const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+import sampleData from "./db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 
 const HomePage = async () => {
 	// await delay(2000);
+	console.log("🚀 ~ sampleData:", sampleData);
+
 	return (
 		<>
-			<h1 className="my-9">hello</h1>
-			<Button className="destructive">buttodn</Button>
+			<ProductList
+				data={sampleData.products}
+				title="Newest Arrivals"
+				limit={4}
+			/>
 		</>
 	);
 };
