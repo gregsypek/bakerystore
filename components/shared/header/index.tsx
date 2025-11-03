651a3912a54076dbb485df6ee1a570363b1f843d
Greg <47268138+gregsypek@users.noreply.github.com>
2025-11-03 16:54:25 +0100

    Responsive Sheet Menu from shadcn

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,44 +1,31 @@
-import { APP_NAME } from '@/lib/constants';
-import {ShoppingCart, UserIcon} from 'lucide-react'
-import Image from 'next/image';
-import Link from 'next/link';
-import { Button } from '../../ui/button';
-import ModeToggle from './mode-toggle';
-
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "./menu";
 
 const Header = () => {
+	return (
+		<header className="w-full border-b">
+			<div className="wrapper flex-between ">
+				<div className="flex-start">
+					<Link href="/" className="flex-start">
+						{/* In Next.js, the priority tag in the Image component is used to enhance the loading behavior of images that are critical for the initial rendering of a page, ensuring they load as quickly as possible. */}
+						<Image
+							src="/images/logo.svg"
+							alt={`${APP_NAME} logo`}
+							height={48}
+							width={48}
+							priority={true}
+						/>
+						<span className="hidden lg:block font-bold text-2xl ml-3">
+							{APP_NAME}
+						</span>
+					</Link>
+				</div>
+				<Menu />
+			</div>
+		</header>
+	);
+};
 
-  return (
-  <header className='w-full border-b'>
-    <div className="wrapper flex-between">
-      
-      <div className="flex-start">
-        
-        <Link href="/" className='flex-start'>
-           {/* In Next.js, the priority tag in the Image component is used to enhance the loading behavior of images that are critical for the initial rendering of a page, ensuring they load as quickly as possible. */}
-          <Image src='/images/logo.svg' alt={`${APP_NAME} logo`} height={48} width={48} priority={true}/>
-          <span className="hidden lg:block font-bold text-2xl ml-3">
-            {APP_NAME}
-          </span>
-        </Link>
-      </div>
-      <div className="space-x-2">
-        <ModeToggle />
-        <Button asChild variant="ghost">
-          <Link href='/cart'>
-          <ShoppingCart /> Cart
-          </Link>
-        </Button>
-        <Button asChild >
-          <Link href='/sign-in'>
-          <UserIcon /> Sign In
-          </Link>
-        </Button>
-      </div>
-    </div>
-
-  </header>
-  );
-}
- 
 export default Header;
