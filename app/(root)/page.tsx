4ee24aa53b878b7af316fc7fcb8a0ae7ddcc62fd
Greg <47268138+gregsypek@users.noreply.github.com>
2025-11-05 16:32:13 +0100

    Load Products from Database using Prisma

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,17 @@
-import { Button } from "@/components/ui/button";
-
 // const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-import sampleData from "./db/sample-data";
+// import sampleData from "../../db/sample-data";
 import ProductList from "@/components/shared/product/product-list";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
 const HomePage = async () => {
+	const latestProducts = await getLatestProducts();
 	// await delay(2000);
-	console.log("🚀 ~ sampleData:", sampleData);
+	// console.log("🚀 ~ sampleData:", sampleData);
 
 	return (
 		<>
-			<ProductList
-				data={sampleData.products}
-				title="Newest Arrivals"
-				limit={4}
-			/>
+			<ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
 		</>
 	);
 };
