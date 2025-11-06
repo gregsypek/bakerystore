411ea58028728de4632042c64ee457cfc907c39d
Greg <47268138+gregsypek@users.noreply.github.com>
2025-11-06 16:18:32 +0100

    Add Neon database support and Prisma adapter for Serverless Environment config via WebSocket

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,13 +1,14 @@
 "use server"
 
+import { prisma } from "@/db/prisma";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
-import {  PrismaClient } from "../generated/prisma/client"
+// import {  PrismaClient } from "../generated/prisma/client"
 import { convertToPlainObject } from "../utils";
 
 // Get latest products
 
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
+  // const prisma = new PrismaClient();
 
   const data = await prisma.product.findMany({take:LATEST_PRODUCTS_LIMIT, orderBy: { createdAt: 'desc' }});
   return convertToPlainObject(data);
