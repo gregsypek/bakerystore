1c7aba3ca0cb98a16fe6f9acd21950637142813e
Greg <47268138+gregsypek@users.noreply.github.com>
2025-11-07 17:43:25 +0100

    Deploy preparation

--- lib/validators.ts
+++ lib/validators.ts
@@ -14,7 +14,7 @@ const currency = z.string().refine((value) => /^\d+(\.\d{2})?$/.test(formatNumbe
 export const insertProductSchema = z.object({
   name: z.string().min(3, "Product name must be at least 3 characters long"),
   slug: z.string().min(3, "Product slug must be at least 3 characters long"),
-  categorY: z.string().min(3, "Category must be at least 3 characters long"),
+  category: z.string().min(3, "Category must be at least 3 characters long"),
   brand: z.string().min(2, "Brand must be at least 2 characters long"),
   description: z.string().min(10, "Description must be at least 10 characters long"),
   stock: z.coerce.number(),
