2ac321b2345d91fcd4598f862630cb3aaa65ea09
Greg <47268138+gregsypek@users.noreply.github.com>
2025-11-20 17:18:18 +0100

    Refactor authentication and cart handling logic to improve session management

--- auth.ts
+++ auth.ts
@@ -6,73 +6,76 @@ import { compareSync } from "bcrypt-ts-edge";
 import { authConfig } from "./auth.config";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  ...authConfig,
-  adapter: PrismaAdapter(prisma), // Łączy NextAuth z bazą danych przez Prisma. Automatycznie zarządza tabelami: User, Account, Session, VerificationToken
-  session: {
-    // This is because this value has to be one of 3 values ("jwt" | "database" | undefined) and we want to make sure it's always "jwt" and not a string that could be anything. Here, strategy is no longer inferred as string; it is inferred as the literal type "jwt".
-    strategy: 'jwt' as const,
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-  },
-  providers: [
-    CredentialsProvider({
-      credentials: {
-        email: { type: "email" },
-        password: { type: "password" }
-      },
-      async authorize(credentials) {
-        if (credentials == null) return null;
+	...authConfig,
+	adapter: PrismaAdapter(prisma), // Łączy NextAuth z bazą danych przez Prisma. Automatycznie zarządza tabelami: User, Account, Session, VerificationToken
+	session: {
+		// This is because this value has to be one of 3 values ("jwt" | "database" | undefined) and we want to make sure it's always "jwt" and not a string that could be anything. Here, strategy is no longer inferred as string; it is inferred as the literal type "jwt".
+		strategy: "jwt" as const,
+		maxAge: 30 * 24 * 60 * 60, // 30 days
+	},
+	providers: [
+		CredentialsProvider({
+			credentials: {
+				email: { type: "email" },
+				password: { type: "password" },
+			},
+			async authorize(credentials) {
+				if (credentials == null) return null;
 
-         // Find user in database
-        const user = await prisma.user.findFirst({
-          where: { email: credentials.email as string }
-        });
-// Check if user exists and password matches
-        if (user && user.password) {
-          const isMatch = compareSync(credentials.password as string, user.password);
-          // If password is correct, return user object
-          if (isMatch) {
-            return {
-              id: user.id,
-              name: user.name,
-              email: user.email,
-              role: user.role
-            };
-          }
-        }
-// If user does not exist or password is incorrect, return null
-        return null;
-      }
-    })
-  ],
-  callbacks: {
-    ...authConfig.callbacks,
-    async session({ session, token, trigger }: any) {
-      session.user.id = token.sub;
-      session.user.role = token.role;
-      session.user.name = token.name;
-      // If there is an update, set the user name
-      if (trigger === 'update') {
-        session.user.name = token.name;
-      }
-      
-      return session;
-    },
-    async jwt({ token, user }: any) {
-      if (user) {
-      // If user has no name then use the email
-        token.role = user.role;
-        
-        if (user.name === 'NO_NAME') {
-          token.name = user.email!.split('@')[0];
-        // Update database to reflect the token name
-          await prisma.user.update({
-            where: { id: user.id },
-            data: { name: token.name }
-          });
-        }
-      }
-      
-      return token;
-    },
-  }
+				// Find user in database
+				const user = await prisma.user.findFirst({
+					where: { email: credentials.email as string },
+				});
+				// Check if user exists and password matches
+				if (user && user.password) {
+					const isMatch = compareSync(
+						credentials.password as string,
+						user.password
+					);
+					// If password is correct, return user object
+					if (isMatch) {
+						return {
+							id: user.id,
+							name: user.name,
+							email: user.email,
+							role: user.role,
+						};
+					}
+				}
+				// If user does not exist or password is incorrect, return null
+				return null;
+			},
+		}),
+	],
+	callbacks: {
+		...authConfig.callbacks,
+		async session({ session, token, trigger }: any) {
+			session.user.id = token.sub;
+			session.user.role = token.role;
+			session.user.name = token.name;
+			// If there is an update, set the user name
+			if (trigger === "update") {
+				session.user.name = token.name;
+			}
+
+			return session;
+		},
+		async jwt({ token, user }: any) {
+			if (user) {
+				// If user has no name then use the email
+				token.role = user.role;
+
+				if (user.name === "NO_NAME") {
+					token.name = user.email!.split("@")[0];
+					// Update database to reflect the token name
+					await prisma.user.update({
+						where: { id: user.id },
+						data: { name: token.name },
+					});
+				}
+			}
+
+			return token;
+		},
+	},
 });
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,3 +1,4 @@
+import { compareSync } from 'bcrypt-ts-edge';
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Bakery Store";
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESC ||  "A modern ecomerce platform for bakery products.";
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL ||
@@ -6,8 +7,8 @@ export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL ||
 export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
 export const signInDefaultValues = {
-  email: "",
-  password: "",
+  email: "admin@example.com",
+  password: "123456",
 };
 
 
