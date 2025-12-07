e0945dc81cbb7ea9709619328baa1e79377cfede
Greg <47268138+gregsypek@users.noreply.github.com>
2025-12-07 12:58:03 +0100

    Add computed fields for cart and order prices in Prisma client

--- db/prisma.ts
+++ db/prisma.ts
@@ -12,6 +12,8 @@ const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaNeon({ connectionString });
  
 // Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
+
+// NOTE: metoda $extends() z opcją result, pozwala transformować dane zwracane z bazy przed ich użyciem w aplikacji.
 export const prisma = new PrismaClient({ adapter }).$extends({
   result: {
     product: {
@@ -26,5 +28,64 @@ export const prisma = new PrismaClient({ adapter }).$extends({
         },
       },
     },
+    cart:{
+      itemsPrice:{
+        needs: {itemsPrice: true},// "Hej Prisma, MUSISZ pobrać to pole!" "needs" Potrzebne gdy pole jest w relacji lub jest opcjonalne!
+        compute(cart){
+          return cart.itemsPrice.toString()
+        }
+      },
+      shippingPrice:{
+        needs: {shippingPrice: true},
+        compute(cart){
+          return cart.shippingPrice.toString()
+        }
+      },
+      taxPrice:{
+        needs: {taxPrice: true},
+        compute(cart){
+          return cart.taxPrice.toString()
+        }
+      },
+      totalPrice:{
+        needs: {totalPrice: true},
+        compute(cart){
+          return cart.totalPrice.toString()
+        }
+      }
+    },
+    order:{
+      itemsPrice:{
+        needs: {itemsPrice: true},
+        compute(cart){
+          return cart.itemsPrice.toString()
+        }
+      },
+      shippingPrice:{
+        needs: {shippingPrice: true},
+        compute(cart){
+          return cart.shippingPrice.toString()
+        }
+      },
+      taxPrice:{
+        needs: {taxPrice: true},
+        compute(cart){
+          return cart.taxPrice.toString()
+        }
+      },
+      totalPrice:{
+        needs: {totalPrice: true},
+        compute(cart){
+          return cart.totalPrice.toString()
+        }
+      }
+    },
+    orderItem:{
+      price: {
+        compute(cart){
+          return cart.price.toString();
+        }
+      }
+    }
   },
 });
