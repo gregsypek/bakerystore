2112c45e2dd947fe58501bf5d5907d8a2d10015f
Greg <47268138+gregsypek@users.noreply.github.com>
2025-12-14 18:29:18 +0100

    Add pagination component and integrate with orders page

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { ZodError } from "zod";
-
+import qs from "query-string";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
@@ -34,7 +34,7 @@ export function formatNumberWithDecimal(num: number): string {
  */
 
 export function formatError(error: unknown): string {
-	console.log("🚀 ~ formatError ~ error:", error)
+	console.log("🚀 ~ formatError ~ error:", error);
 	// 1. Obsługa błędów walidacji Zod
 	if (error instanceof ZodError) {
 		//     ~ formatError ~ messages: [
@@ -53,7 +53,8 @@ export function formatError(error: unknown): string {
 
 	// 2. Obsługa błędów Prisma (sprawdzanie po właściwościach)
 	// Nie używamy instanceof, żeby nie importować Prisma
-	if (error && typeof error === "object" && "code" in error) {//  Operator "in" sprawdza czy klucz istnieje w obiekcie  const obj1 = { code: 'P2002', message: 'Error' }; 'code' in obj1;  // ✅ true
+	if (error && typeof error === "object" && "code" in error) {
+		//  Operator "in" sprawdza czy klucz istnieje w obiekcie  const obj1 = { code: 'P2002', message: 'Error' }; 'code' in obj1;  // ✅ true
 		const prismaError = error as {
 			code: string;
 			meta?: { target?: string[] };
@@ -107,7 +108,7 @@ export function formatError(error: unknown): string {
 export function round2(value: number | string) {
 	if (typeof value === "number") {
 		return Math.round((value + Number.EPSILON) * 100) / 100; // Epsilon to bardzo mała liczba dodawana, aby uniknąć błędów zaokrągleń
-	}else if (typeof value === "string") {
+	} else if (typeof value === "string") {
 		return Math.round((Number(value) + Number.EPSILON) * 100) / 100;
 	} else {
 		throw new Error("Invalid value type");
@@ -125,54 +126,76 @@ const CURRENCY_FORMATTER = new Intl.NumberFormat("en-US", {
 export function formatCurrency(amount: number | string | null) {
 	if (typeof amount === "number") {
 		return CURRENCY_FORMATTER.format(amount);
-	 } else if (typeof amount === "string") {
+	} else if (typeof amount === "string") {
 		return CURRENCY_FORMATTER.format(Number(amount));
-	 } else {
+	} else {
 		// return CURRENCY_FORMATTER.format(0);
-		return 'NaN'
-	 }
+		return "NaN";
+	}
 }
 
 export function formatId(id: string) {
-  return `..${id.substring(id.length - 6)}`;
+	return `..${id.substring(id.length - 6)}`;
 }
 
 // Format date and times
 export const formatDateTime = (dateString: Date) => {
-  const dateTimeOptions: Intl.DateTimeFormatOptions = {
-    month: 'short', // abbreviated month name (e.g., 'Oct')
-    year: 'numeric', // abbreviated month name (e.g., 'Oct')
-    day: 'numeric', // numeric day of the month (e.g., '25')
-    hour: 'numeric', // numeric hour (e.g., '8')
-    minute: 'numeric', // numeric minute (e.g., '30')
-    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
-  };
-  const dateOptions: Intl.DateTimeFormatOptions = {
-    weekday: 'short', // abbreviated weekday name (e.g., 'Mon')
-    month: 'short', // abbreviated month name (e.g., 'Oct')
-    year: 'numeric', // numeric year (e.g., '2023')
-    day: 'numeric', // numeric day of the month (e.g., '25')
-  };
-  const timeOptions: Intl.DateTimeFormatOptions = {
-    hour: 'numeric', // numeric hour (e.g., '8')
-    minute: 'numeric', // numeric minute (e.g., '30')
-    hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
-  };
-  const formattedDateTime: string = new Date(dateString).toLocaleString(
-    'en-US',
-    dateTimeOptions
-  );
-  const formattedDate: string = new Date(dateString).toLocaleString(
-    'en-US',
-    dateOptions
-  );
-  const formattedTime: string = new Date(dateString).toLocaleString(
-    'en-US',
-    timeOptions
-  );
-  return {
-    dateTime: formattedDateTime,
-    dateOnly: formattedDate,
-    timeOnly: formattedTime,
-  };
+	const dateTimeOptions: Intl.DateTimeFormatOptions = {
+		month: "short", // abbreviated month name (e.g., 'Oct')
+		year: "numeric", // abbreviated month name (e.g., 'Oct')
+		day: "numeric", // numeric day of the month (e.g., '25')
+		hour: "numeric", // numeric hour (e.g., '8')
+		minute: "numeric", // numeric minute (e.g., '30')
+		hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+	};
+	const dateOptions: Intl.DateTimeFormatOptions = {
+		weekday: "short", // abbreviated weekday name (e.g., 'Mon')
+		month: "short", // abbreviated month name (e.g., 'Oct')
+		year: "numeric", // numeric year (e.g., '2023')
+		day: "numeric", // numeric day of the month (e.g., '25')
+	};
+	const timeOptions: Intl.DateTimeFormatOptions = {
+		hour: "numeric", // numeric hour (e.g., '8')
+		minute: "numeric", // numeric minute (e.g., '30')
+		hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+	};
+	const formattedDateTime: string = new Date(dateString).toLocaleString(
+		"en-US",
+		dateTimeOptions
+	);
+	const formattedDate: string = new Date(dateString).toLocaleString(
+		"en-US",
+		dateOptions
+	);
+	const formattedTime: string = new Date(dateString).toLocaleString(
+		"en-US",
+		timeOptions
+	);
+	return {
+		dateTime: formattedDateTime,
+		dateOnly: formattedDate,
+		timeOnly: formattedTime,
+	};
 };
+
+// Form the pagination links
+export function formUrlQuery({
+	params,
+	key,
+	value,
+}: {
+	params: string;
+	key: string;
+	value: string | null;
+}) {
+	const query = qs.parse(params);
+	query[key] = value;
+	
+	return qs.stringifyUrl(
+		{
+			url: window.location.pathname,
+			query,
+		},
+		{ skipNull: true }
+	);
+}
