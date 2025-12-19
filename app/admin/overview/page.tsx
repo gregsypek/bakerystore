2b65e4f1f37bc91cea15a1419f0920afa521c719
Greg <47268138+gregsypek@users.noreply.github.com>
2025-12-19 16:00:01 +0100

    Implement admin overview page with session validation and order summary retrieval

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -1,4 +1,19 @@
-const AdminOverviewPage = () => {
+import { auth } from "@/auth";
+import { getOrderSummary } from "@/lib/actions/order.actions";
+import { Metadata } from "next";
+
+export const metadat: Metadata = {
+	title: "Admin Dashboard",
+};
+const AdminOverviewPage = async () => {
+	const session = await auth();
+
+	if (session?.user?.role !== "admin") {
+		throw new Error("User is not authorized");
+	}
+
+	const summary = await getOrderSummary();
+	console.log("🚀 ~ AdminOverviewPage ~ summary:", summary);
 	return <>Overview</>;
 };
 
