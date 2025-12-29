1ff64f4ed16a30ef11e48f4d890d216167140412
Greg Sypek <greg.sypek@gmail.com>
2025-12-29 20:50:22 +0100

    Add CategoryDrawer component and implement drawer functionality for category selection

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -2,13 +2,15 @@ import { APP_NAME } from '@/lib/constants';
 import Image from 'next/image';
 import Link from 'next/link';
 import Menu from './menu';
+import CategoryDrawer from './category-drawer';
 
 const Header = () => {
 	return (
 		<header className="w-full border-b">
 			<div className="wrapper flex-between ">
 				<div className="flex-start">
-					<Link href="/" className="flex-start">
+					<CategoryDrawer />
+					<Link href="/" className="flex-start ml-4">
 						{/* In Next.js, the priority tag in the Image component is used to enhance the loading behavior of images that are critical for the initial rendering of a page, ensuring they load as quickly as possible. */}
 						<Image
 							src="/images/logo.svg"
