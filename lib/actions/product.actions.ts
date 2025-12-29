908ad02b6334f983c5e73faf08e451aa972c737f
Greg Sypek <greg.sypek@gmail.com>
2025-12-29 18:17:55 +0100

    Add search functionality to admin products page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -7,6 +7,7 @@ import { convertToPlainObject, formatError } from "../utils";
 import { revalidatePath } from "next/cache";
 import z from "zod";
 import { insertProductSchema, updateProductSchema } from "../validators";
+import { Prisma } from "../generated/prisma/client";
 
 // Get latest products
 export async function getLatestProducts() {
@@ -38,7 +39,6 @@ export async function getProductById(productId: string) {
 export async function getAllProducts({
 	query,
 	limit = PAGE_SIZE,
-	// limit = 2,
 	page,
 	category,
 }: {
@@ -46,8 +46,19 @@ export async function getAllProducts({
 	limit?: number;
 	page: number;
 	category?: string;
-}) {
+	}) {
+		const queryFilter: Prisma.ProductWhereInput = query && query !== 'all' ? {
+
+			name: {
+				contains: query,
+				mode: 'insensitive'
+			} as Prisma.StringFilter
+	
+	} : {}
 	const data = await prisma.product.findMany({
+		where: {
+			...queryFilter
+		},
 		orderBy: { createdAt: "desc" },
 		skip: (page - 1) * limit,
 		take: limit,
