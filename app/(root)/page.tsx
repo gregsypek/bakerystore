8667d7f3a74f86a7c25d28b4a53103d318ca8daa
Greg Sypek <greg.sypek@gmail.com>
2025-12-30 22:11:36 +0100

    Add View All Products button and enhance SearchPage with product listing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,7 @@
 // import sampleData from "../../db/sample-data";
 import ProductCarousel from '@/components/shared/product/product-carousel';
 import ProductList from '@/components/shared/product/product-list';
+import ViewAllProductsButton from '@/components/view-all-products-button';
 import {
 	getFeaturedProducts,
 	getLatestProducts,
@@ -20,6 +21,7 @@ const HomePage = async () => {
 				<ProductCarousel data={featuredProducts} />
 			)}
 			<ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
+			<ViewAllProductsButton />
 		</>
 	);
 };
