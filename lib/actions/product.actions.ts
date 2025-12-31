3758e11be2c54ef37c4252e22893bc292d3bb323
Greg Sypek <greg.sypek@gmail.com>
2025-12-31 16:18:02 +0100

    Add more search filters into getAllProducts

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -91,9 +91,38 @@ export async function getAllProducts({
 					} as Prisma.StringFilter,
 				}
 			: {};
+	console.log('🚀 ~ getAllProducts ~ queryFilter:', queryFilter);
+
+	// Category filter
+	const categoryFilter = category && category !== 'all' ? { category } : {};
+
+	// Price filter
+	const priceFilter: Prisma.ProductWhereInput =
+		price && price !== 'all'
+			? {
+					price: {
+						gte: Number(price.split('-')[0]),
+						lte: Number(price.split('-')[1]),
+					},
+				}
+			: {};
+
+	// Rating filter
+	const ratingFilter =
+		rating && rating !== 'all'
+			? {
+					rating: {
+						gte: Number(rating),
+					},
+				}
+			: {};
+
 	const data = await prisma.product.findMany({
 		where: {
 			...queryFilter,
+			...categoryFilter,
+			...priceFilter,
+			...ratingFilter,
 		},
 		orderBy: { createdAt: 'desc' },
 		skip: (page - 1) * limit,
