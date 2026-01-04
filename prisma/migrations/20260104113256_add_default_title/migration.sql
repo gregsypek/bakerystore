c8f5f9dc7d652720dd62452f920ee37615eeedc3
Greg Sypek <greg.sypek@gmail.com>
2026-01-04 12:35:20 +0100

    Add default title into Review model and change from optional into required

---
+++ prisma/migrations/20260104113256_add_default_title/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Made the column `title` on table `Review` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "Review" ALTER COLUMN "title" SET NOT NULL,
+ALTER COLUMN "title" SET DEFAULT 'Review';
